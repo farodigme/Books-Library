e7447963c6d8b1f1606fa0647472950b10c58b7a
Daniel <54008118+Winjoker@users.noreply.github.com>
2024-12-13 23:01:11 +0300

    Started

---
+++ BooksLibrary/Models/DataBase/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLibrary.Models.DataBase
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+           : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;Database=bookslibrary;User=root;Password=1234;",
+                    new MySqlServerVersion(new Version(8, 0, 33)));
+            }
+        }
+    }
+}
