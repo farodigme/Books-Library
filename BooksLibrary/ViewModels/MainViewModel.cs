11ae390789ba6025219f32b1a0aa5ad7abf24b45
Daniel <54008118+Winjoker@users.noreply.github.com>
2024-12-16 20:16:52 +0300

    Add Reports Functional

--- BooksLibrary/ViewModels/MainViewModel.cs
+++ BooksLibrary/ViewModels/MainViewModel.cs
@@ -2,17 +2,23 @@
 using BooksLibrary.Utils;
 using BooksLibrary.Views;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
+using static MaterialDesignThemes.Wpf.Theme;
+using static System.Reflection.Metadata.BlobBuilder;
 
 namespace BooksLibrary.ViewModels
 {
@@ -542,6 +548,94 @@ namespace BooksLibrary.ViewModels
 					  {
 						  Storage.Add(s);
 					  }
+				  }));
+			}
+		}
+
+		private RelayCommand? _createReportCommand;
+		public RelayCommand CreateReportCommand
+		{
+			get
+			{
+				return _createReportCommand ??
+				  (_createReportCommand = new RelayCommand((parameter) =>
+				  {
+					  if (parameter == null) return;
+
+					  var saveFileDialog = new SaveFileDialog
+					  {
+						  Filter = "Excel Files|*.xlsx",
+						  Title = "Сохранить Excel файл",
+						  FileName = "Export.xlsx"
+					  };
+
+					  if (saveFileDialog.ShowDialog() == true)
+					  {
+						  try
+						  {
+							  // Создание Excel файла
+							  using (var package = new ExcelPackage())
+							  {
+								  var worksheet = package.Workbook.Worksheets.Add("Отчет");
+
+								  int row = 2;
+								  if (parameter.ToString() == "Rentals")
+								  {
+									  worksheet.Cells[1, 1].Value = "Название книги";
+									  worksheet.Cells[1, 2].Value = "Фамилия студента";
+									  worksheet.Cells[1, 3].Value = "Имя студента";
+									  worksheet.Cells[1, 4].Value = "Название факультета";
+									  worksheet.Cells[1, 5].Value = "Дата выдачи";
+									  worksheet.Cells[1, 6].Value = "Дата возврата";
+									  worksheet.Cells[1, 7].Value = "Возвращено";
+									  worksheet.Cells[1, 1, 1, 7].Style.Font.Bold = true;
+
+									  foreach (var item in Rentals)
+									  {
+										  worksheet.Cells[row, 1].Value = item.Book.Title;
+										  worksheet.Cells[row, 2].Value = item.Student.LastName;
+										  worksheet.Cells[row, 3].Value = item.Student.FirstName;
+										  worksheet.Cells[row, 4].Value = item.Student.Group.Speciality.Faculty.Name;
+										  worksheet.Cells[row, 5].Value = item.RentalDate.ToString("yyyy-MM-dd");
+										  worksheet.Cells[row, 6].Value = item.ReturnDate?.ToString("yyyy-MM-dd");
+										  worksheet.Cells[row, 7].Value = item.isReturned;
+										  row++;
+									  }
+								  }
+
+								  if (parameter.ToString() == "Storage")
+								  {
+									  worksheet.Cells[1, 1].Value = "Название книги";
+									  worksheet.Cells[1, 2].Value = "Наименование зала";
+									  worksheet.Cells[1, 3].Value = "Местонахождение";
+									  worksheet.Cells[1, 4].Value = "Количество";
+									  worksheet.Cells[1, 1, 1, 4].Style.Font.Bold = true;
+
+									  foreach (var item in Storage)
+									  {
+										  worksheet.Cells[row, 1].Value = item.Book.Title;
+										  worksheet.Cells[row, 2].Value = item.Branch.Name;
+										  worksheet.Cells[row, 3].Value = item.Branch.Location;
+										  worksheet.Cells[row, 4].Value = item.Count;
+
+										  row++;
+									  }
+								  }
+
+								  worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+								  var fileInfo = new FileInfo(saveFileDialog.FileName);
+								  package.SaveAs(fileInfo);
+
+								  System.Windows.MessageBox.Show("Отчет сформирован успешно", "Информация");
+							  }
+						  }
+						  catch (Exception ex)
+						  {
+							  System.Windows.MessageBox.Show($"Ошибка формирования отчета: {ex.Message}", "Ошибка");
+						  }
+					  }
+
 				  }));
 			}
 		}
